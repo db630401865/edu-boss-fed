6cf4d2db0826b85bfd9c944e5a96b95d946d27bb
bo.dong <bo.dong@bangcle.com>
2021-08-24 10:59:12 +0800

    Vue综合模块6

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="Welcome to Your Vue.js 1+ TypeScript App"/>
   </div>
 </template>
 
